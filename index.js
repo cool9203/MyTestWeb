29d6b1da4f667bada39a1204edcf6a666501943d
cool9203 <octer18@gmail.com>
2019-11-20 04:17:08 -0800

    test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-document.write("<br>this is js test string<br>")
+var botton - document.querySelector('botton')
+botton.addEventListener('click', botton_event)
+
+function botton_event(){
+	alert("test botton event.")
+}
