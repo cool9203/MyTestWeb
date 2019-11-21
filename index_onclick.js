851007cd621b7016cb6b13b9b40231b006a8a03e
cool9203 <octer18@gmail.com>
2019-11-21 18:21:06 +0800

    test ajax

--- index_onclick.js
+++ index_onclick.js
@@ -5,38 +5,39 @@ function XMLHttpRequest_event(){
 			show(null,null,this);
 		}
 		else{
-			document.querySelector('.ajaxshow').innerHTML = "False"
+			document.querySelector('.ajaxshow').innerHTML = "False";
 		}
 	};
-	xhr.open('post', "https://ip-api.com/json");
+	xhr.open('get', "https://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=1000&%24format=json");
 	xhr.send();
 }
 
 function JQueryajax_event(){
     $.ajax({
-        url:"https://ip-api.com/json",
+        url:"https://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=1000&%24format=json",
         dataType:"json",
         async:true,
         data:{"id":"value"},
-        type:"post",
+        type:"get",
         success:show,
         error:()=>document.querySelector('.ajaxshow').innerHTML = "False"
     });
 }
 
 function show(data, status, jqxhr){
-    clear_event()
+    clear_event();
     if (jqxhr["responseText"] != null){
         result = JSON.parse(jqxhr.responseText);
     }
     else{
         return 0;
     }
+    document.querySelector('.ajaxshow').innerHTML += "<div class=\"data_title\">空氣品質AQI</div><br>資料時間:" + result["0"]["PublishTime"] + "<br>";
 	for (var x in result){
-		document.querySelector('.ajaxshow').innerHTML += x + ":" + result[x] + "<br>"
+		document.querySelector('.ajaxshow').innerHTML += result[x]["SiteName"] + ":" + result[x]["Status"] + "<br>";
     }
 }
 
 function clear_event(){
-    document.querySelector('.ajaxshow').innerHTML = ""
+    document.querySelector('.ajaxshow').innerHTML = "";
 }
