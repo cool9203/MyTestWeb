b652d08c0b6ffb655b997ebc4217d096b0d80ae5
cool9203 <octer18@gmail.com>
2019-11-21 12:15:18 +0800

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var botton = document.getElementById('botton')
-botton.addEventListener('click', botton_event)
+var botton1 = document.querySelectorAll('.botton1')
+for (let i=0;i<botton1.length;i++){
+	botton1[i].addEventListener('click', botton1_event)
+}
 
-function botton_event(){
+function botton1_event(){
 	alert("test botton event.")
 }
